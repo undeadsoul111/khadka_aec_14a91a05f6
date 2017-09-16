51490b5f7a4b916ca7b1540760768aa3799708a7
undeadsoul111 <undeadsoul111@gmail.com>
2017-09-16 16:26:08 +0530

    commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+package javaproject;
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+	}
+
+}
